8b13b2b2f83f058d7fb21963363693dda9cd229d
Ayush Singh <ayushsingh2442@gmail.com>
2023-12-13 19:36:22 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-      
+        <p>Button clicked {count} times</p>
+    <button onClick={()=> onClick(count+1)}>Click me</button>
     </div>
   )
 }
